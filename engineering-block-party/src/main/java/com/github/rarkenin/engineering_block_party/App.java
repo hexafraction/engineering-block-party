661bc02560b45d1a0017a024b9e030242fa500cf
hexafraction <hexafraction@gmail.com>
2013-10-02 14:01:38 -0400

    Initial project commit

---
+++ engineering-block-party/src/main/java/com/github/rarkenin/engineering_block_party/App.java
@@ -0,0 +1,13 @@
+package com.github.rarkenin.engineering_block_party;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
