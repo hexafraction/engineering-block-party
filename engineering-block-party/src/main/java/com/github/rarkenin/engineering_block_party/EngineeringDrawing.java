68478c5f02b0d3a2ae16440160736cda82fb7625
hexafraction <hexafraction@gmail.com>
2013-10-04 13:56:27 -0400

    Add more line and block objects, use inheritance, rename Face to View.

--- engineering-block-party/src/main/java/com/github/rarkenin/engineering_block_party/EngineeringDrawing.java
+++ engineering-block-party/src/main/java/com/github/rarkenin/engineering_block_party/EngineeringDrawing.java
@@ -1,28 +1,35 @@
 package com.github.rarkenin.engineering_block_party;
 
+import java.util.ArrayList;
+
 public class EngineeringDrawing {
 
-	
-	public static class Dimension {
+	public class Cube {
+		public long x, y, z;
+	}
+	public static class Line{
+		public Point p1;
+		public Point p2;
+		public View view;
+		public Orientation orientation;
+		public long lengthTicks;
+	}
+	public static class Dimension extends Line{
 		public EngineeringDrawing.ExtensionLine ext1;
 		public EngineeringDrawing.ExtensionLine ext2;
 		public long lengthTicks;
 		public EngineeringDrawing.Orientation orientation;
 	}
 
-	public static class ObjectLine {
-		public Face face;
-		public Orientation orientation;
-		public EngineeringDrawing.Point pt1;
-		public EngineeringDrawing.Point pt2;
-		public long lengthTicks;
-	}
-
-	public static class ExtensionLine {
+	public static class ObjectLine extends Line{
 
 	}
 
-	public static class HiddenLine {
+	public static class ExtensionLine extends Line {
+		
+	}
+
+	public static class HiddenLine extends Line{
 
 	}
 
@@ -31,17 +38,25 @@ public class EngineeringDrawing {
 	}
 
 	public class Point {
-		Face face;
+		View view;
 		public long ticksX;
 		public long ticksY;
+		ArrayList<Line> lines;
 	}	
 
 	
-	public enum Face {
+	public enum View {
 		FACE_FRONT, FACE_TOP, FACE_RIGHT
 	}
 
 	public enum Orientation {
 		VERTICAL, HORIZONTAL
+	}
+	
+	public static class Face{
+		long zindex;
+		ArrayList<ObjectLine> objectLines;
+		ArrayList<Dimension> dimension;
+		ArrayList<Cube> cubes;
 	}
 }
